441fe75a420844d86721725869659e8b658a4266
dubbyhub <devhelms2015@gmail.com>
2024-12-16 08:28:36 -0600

    Update leagueInfo.js

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -6,13 +6,13 @@ export const dynasty = true; // true for dynasty leagues, false for redraft and 
 export const enableBlog = false; // requires VITE_CONTENTFUL_ACCESS_TOKEN and VITE_CONTENTFUL_SPACE environment variables
 
 /*   STEP 2   */
-// export const homepageText = `
-//   <p>Founded in 2017, the Good Ole Dynasty League prides itself as being one of the world's most interactive dynasty leagues in the world. Headquartered in New Orleans, Louisiana, the GODL consists of 12 franchises, most of which can be found in the heart of Acadiana in the pelican state. The lone exception being our ATL expansion team Chief Beef. </p>
-//   <p>Our league hosts multiple events throughout the fantasy regular season and off-season. Most notably the GODL Rookie Draft, where (hopefully) all 12 General Managers meet at a central location to welcome in the next generation of fantasy superstars. 4 Rounds, 48 picks, 3 Hours of sperg, 1 rap diss track, and if we're lucky some mild hog crankin'.</p>
-//   <p>The Good Ole Dynasty league hosts its primary communications through the Discord app. There, we take part in daily communications covering a magnitude of different topics. Multiple servers for different sports, video games and entertainment, and the very sacred Chat Rankings. Send a funny message? Congrats, you've earned yourself 6 skulls resulting in 12 chat points. Thirsting over Sydney Sweeney's latest insta post? Uh-Oh. 12 Horny bonks for you leaving you a whopping -60 on your total points. Get some water you pig.</p>
-//   <p>Don't let the fun and games distract you from the one true purpose of the GODL. We at the GODL al have one central goal in mind. Becoming a champion. We put countless hours into draft prep, lineup decisions, and trade negotiations. All in hopes of one day being the last one standing.</p>
-//   <p>Thanks for stopping by on the Good Ole Dynasty League Official Webpage. Don't forget to check out all the tabs under the League Info section. If you'd like to suggest any edits to this page, you can reach out to Dev on discord, or by his X (formerly twitter) account @devhelms.</p>
-// `;
+export const homepageText = `
+  <p>Founded in 2017, the Good Ole Dynasty League prides itself as being one of the world's most interactive dynasty leagues in the world. Headquartered in New Orleans, Louisiana, the GODL consists of 12 franchises, most of which can be found in the heart of Acadiana in the pelican state. The lone exception being our ATL expansion team Chief Beef. </p>
+`;
+  // <p>Our league hosts multiple events throughout the fantasy regular season and off-season. Most notably the GODL Rookie Draft, where (hopefully) all 12 General Managers meet at a central location to welcome in the next generation of fantasy superstars. 4 Rounds, 48 picks, 3 Hours of sperg, 1 rap diss track, and if we're lucky some mild hog crankin'.</p>
+  // <p>The Good Ole Dynasty league hosts its primary communications through the Discord app. There, we take part in daily communications covering a magnitude of different topics. Multiple servers for different sports, video games and entertainment, and the very sacred Chat Rankings. Send a funny message? Congrats, you've earned yourself 6 skulls resulting in 12 chat points. Thirsting over Sydney Sweeney's latest insta post? Uh-Oh. 12 Horny bonks for you leaving you a whopping -60 on your total points. Get some water you pig.</p>
+  // <p>Don't let the fun and games distract you from the one true purpose of the GODL. We at the GODL al have one central goal in mind. Becoming a champion. We put countless hours into draft prep, lineup decisions, and trade negotiations. All in hopes of one day being the last one standing.</p>
+  // <p>Thanks for stopping by on the Good Ole Dynasty League Official Webpage. Don't forget to check out all the tabs under the League Info section. If you'd like to suggest any edits to this page, you can reach out to Dev on discord, or by his X (formerly twitter) account @devhelms.</p>
 
 /*   STEP 3   */
 /*
